ef7eb5aaf37430504c468098ce83347201a3face
menor <josemenor@gmail.com>
2015-07-28 08:51:44 +0200

    Refactor availabilities store to see if it passes the test

--- app/js/__tests__/AvailabilitiesStore-test.js
+++ app/js/__tests__/AvailabilitiesStore-test.js
@@ -6,7 +6,8 @@ import AvailabilitiesStore from '../stores/AvailabilitiesStore';
 import AppDispatcher from '../dispatchers/AppDispatcher';
 
 const fixtures = {
-  availabilities: [1, 2, 3]
+  availabilities: [1, 2, 3],
+  covers: 5
 };
 
 test('Availabilities Store updates after an AVAILABILITIES_LOADED action', (assert) => {
@@ -23,3 +24,17 @@ test('Availabilities Store updates after an AVAILABILITIES_LOADED action', (asse
     'The store gets the Availabilities from the dispatcher');
   assert.end();
 });
+
+test('Covers in Availabilities Store update after a NUMBER_OF_COVERS_CHANGED action', (assert) => {
+  AppDispatcher.dispatch({
+    source: PayloadSources.VIEW_ACTION,
+    action: { actionType: ActionTypes.NUMBER_OF_COVERS_CHANGED,
+              newCoverValue: fixtures.covers }
+  });
+
+  const expected = fixtures.covers;
+
+  assert.equal(AvailabilitiesStore.getState().covers, expected,
+    'The store updates its cover value');
+  assert.end();
+})
