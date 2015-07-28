d756977dc186cc2326831463e744c7b77fcea4ec
menor <josemenor@gmail.com>
2015-07-28 09:57:56 +0200

    Rewrite the test to use new fixtures every time. Still not passing.

--- app/js/__tests__/AvailabilitiesStore-test.js
+++ app/js/__tests__/AvailabilitiesStore-test.js
@@ -1,40 +1,60 @@
 import React from 'react';
-import {test} from 'tape';
+import test from 'tape';
 
 import { PayloadSources, ActionTypes } from '../constants/Constants';
 import AvailabilitiesStore from '../stores/AvailabilitiesStore';
 import AppDispatcher from '../dispatchers/AppDispatcher';
 
-const fixtures = {
-  availabilities: [1, 2, 3],
-  covers: 5
+const before = test;
+const after = test;
+
+const setup = () => {
+  const fixtures = {};
+  fixtures.availabilities = [1, 2, 3];
+  fixtures.covers = 5;
+
+  return fixtures;
 };
 
-test('Availabilities Store updates after an AVAILABILITIES_LOADED action', (assert) => {
+const teardown = (fixtures) => {
+  delete fixtures.availabilities;
+  delete fixtures.covers;
+};
+
+test('Availabilities Store availabilities update after an AVAILABILITIES_LOADED action', (assert) => {
+  const fixture = setup();
+
   AppDispatcher.dispatch({
     source: PayloadSources.SERVER_ACTION,
     action: { actionType: ActionTypes.AVAILABILITES_LOADED,
-              availabilities: fixtures.availabilities }
+              availabilities: fixture.availabilities }
   });
 
-  const expected = fixtures.availabilities;
+  const expected = fixture.availabilities;
   const actual = AvailabilitiesStore.getState().availabilities;
 
   assert.deepEqual(actual, expected,
-    'The store gets the Availabilities from the dispatcher');
+    'The store should get the Availabilities from the dispatcher');
+
+  teardown(fixture);
   assert.end();
 });
 
-test('Covers in Availabilities Store update after a NUMBER_OF_COVERS_CHANGED action', (assert) => {
+
+test('Availabilities Store covers update after a NUMBER_OF_COVERS_CHANGED action', (assert) => {
+  const fixture = setup();
+
   AppDispatcher.dispatch({
     source: PayloadSources.VIEW_ACTION,
     action: { actionType: ActionTypes.NUMBER_OF_COVERS_CHANGED,
-              newCoverValue: fixtures.covers }
+              newCoverValue: fixture.covers }
   });
 
-  const expected = fixtures.covers;
+  const expected = fixture.covers;
 
   assert.equal(AvailabilitiesStore.getState().covers, expected,
-    'The store updates its cover value');
+    'The store should update its covers value after a NUMBER_OF_COVERS_CHANGED action');
+
+  teardown(fixture);
   assert.end();
 })
