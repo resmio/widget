eaef3941b6f6ccd3842509659286a07df2986159
menor <josemenor@gmail.com>
2015-08-12 20:26:45 +0200

    Create GuestPanel placeholder

--- app/js/App.jsx
+++ app/js/App.jsx
@@ -5,6 +5,7 @@ import formatDateForApi from './utils/formatDateForApi';
 // Components
 import WidgetHeader from './components/WidgetHeader';
 import AvailabilitiesPanel from './components/AvailabilitiesPanel';
+import GuestPanel from './components/GuestPanel';
 import PanelSwitcher from './components/PanelSwitcher';
 
 export default class App extends React.Component {
@@ -37,7 +38,7 @@ export default class App extends React.Component {
         {(() => {
           switch (this.state.showPanel) {
             case 1: return availabilitiesPanel;
-            case 2: return '';
+            case 2: return <GuestPanel />;
             case 3: return '';
             default: return availabilitiesPanel;
           }
---
+++ app/js/components/GuestPanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import AvailabilitiesStore from '../stores/AvailabilitiesStore';
+// import ViewActionCreators from '../actions/ViewActionCreators';
+
+// Components
+// import PersonPicker from '../components/PersonPicker';
+
+export default class GuestPanel extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hi I'm panel 2</h1>
+      </div>
+    );
+  }
+}
