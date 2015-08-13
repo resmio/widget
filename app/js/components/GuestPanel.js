8b42b8057632fbf277f19da94a6ed5097400fa7d
menor <josemenor@gmail.com>
2015-08-13 07:42:52 +0200

    Create fake NameInput component

--- app/js/components/GuestPanel.js
+++ app/js/components/GuestPanel.js
@@ -3,14 +3,18 @@ import React from 'react';
 // import ViewActionCreators from '../actions/ViewActionCreators';
 
 // Components
-// import PersonPicker from '../components/PersonPicker';
+import NameInput from './NameInput';
 
 export default class GuestPanel extends React.Component {
 
   render() {
     return (
       <div>
-        <h1>Hi I'm panel 2</h1>
+        <p>Facebook Stuff</p>
+        <NameInput />
+        <input type="phone">Phone</input>
+        <input type="email">Email</input>
+        <input type="checkbox">Newsletter</input>
       </div>
     );
   }
