159e27e463a21ee228351722ee16014662268b4d
menor <josemenor@gmail.com>
2015-08-13 14:26:32 +0200

    Make all components dumb

--- app/components/WidgetHeader.jsx
+++ app/components/WidgetHeader.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import WidgetHeaderTitle from './WidgetHeaderTitle';
 
 export default class WidgetHeader extends React.Component {
 
   render() {
     return (
       <div id="widgetHeader">
-        <WidgetHeaderTitle facilityName={ this.props.facilityName } />
+        <div>
+          <h1>{ this.props.facilityName }</h1>
+          <h2>Online Reservation</h2>
+        </div>
         <h3>{ this.props.reservationCovers } persons</h3>
         <h3>{ this.props.reservationDate }</h3>
         <h3>{ this.props.reservationTimeslot }</h3>
