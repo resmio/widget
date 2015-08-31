b35bd5f2932b346890060b6b8228252d8c60beb6
menor <josemenor@gmail.com>
2015-08-31 09:20:14 +0200

    Make person-picker (kinda) work

--- app/components/PersonPicker.jsx
+++ app/components/PersonPicker.jsx
@@ -5,9 +5,7 @@ import ViewActionCreators from '../actions/ViewActionCreators';
 export default class PersonPicker extends React.Component {
 
   renderPeopleSelector() {
-    const numberElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    return numberElements.map((number) => {
+    return this.state.numberElements.map((number) => {
       return (
         <li
           key={number}
@@ -21,23 +19,40 @@ export default class PersonPicker extends React.Component {
 
   render() {
     let component;
+    let nextButton;
+    let previousButton;
     let classString = 'person-picker--';
     if (this.props.isExpanded) {
-      component = (<ul>
+      component = (<ul onClick={this.handlePersonPickerClick}>
                      {this.renderPeopleSelector()}
-                   </ul>);
+                   </ul>
+                  );
+      nextButton = (
+        <a href="#"
+          onClick={this.handleNextButtonClick}>
+          &#10095;
+        </a>
+      );
+      previousButton = (
+        <a href="#"
+          onClick={this.handlePreviousButtonClick}>
+          &#10094;
+        </a>
+      );
       classString += 'is-expanded';
     }
     if (!this.props.isExpanded) {
-      component = (<span className="person-picker">
+      component = (<span className="person-picker" onClick={this.handlePersonPickerClick}>
                     {this.props.numberOfCovers}
                    </span>);
       classString += 'is-collapsed';
     }
     return (
-      <div className={ classString } onClick={this.handlePersonPickerClick}>
+      <div className={ classString }>
         <span className="component__label">People</span>
+        { previousButton }
         { component }
+        { nextButton }
       </div>
     );
   }
@@ -47,9 +62,12 @@ export default class PersonPicker extends React.Component {
     // We trigger the action to get the availabilities for today from here
     // This will update the state , so we render it properly
     this.state = AvailabilitiesStore.getState();
+    this.state.numberElements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     // We need to bind functions here so this won't refer to React
     // Will be solved in ES7
     this.handleClickOnPeopleNumber = this.handleClickOnPeopleNumber.bind(this);
+    this.handleNextButtonClick = this.handleNextButtonClick.bind(this);
+    this.handlePreviousButtonClick = this.handlePreviousButtonClick.bind(this);
   }
 
   handleClickOnPeopleNumber(number) {
@@ -58,6 +76,20 @@ export default class PersonPicker extends React.Component {
 
   handlePersonPickerClick() {
     ViewActionCreators.changePersonPickerUiState();
+  }
+
+  handleNextButtonClick() {
+    this.state.numberElements = this.state.numberElements.map((number) => {
+      return number + 9;
+    });
+    this.forceUpdate();
+  }
+
+  handlePreviousButtonClick() {
+    this.state.numberElements = this.state.numberElements.map((number) => {
+      return number - 9;
+    });
+    this.forceUpdate();
   }
 }
 
