5d8e78cf35ca34be529b763647f15dfda0fc11e9
menor <josemenor@gmail.com>
2016-11-01 15:33:59 +0100

    Remove everything

--- app/utils/formatDateForApi.js
+++
@@ -1,8 +0,0 @@
-export default function formatDateForApi(date) {
-  const year = date.getFullYear();
-  // To get days and months with two digits, we add a 0 to the left
-  // and pick the last 2 characters from the right
-  const paddedMonth = ('0' + (date.getMonth() + 1)).slice(-2);
-  const paddedDay = ('0' + date.getDate()).slice(-2);
-  return `${year}-${paddedMonth}-${paddedDay}`;
-}
