87a0f2df71dafb775a65b7a59495f252b073a4d0
menor <josemenor@gmail.com>
2016-11-02 07:21:07 +0100

    Set up a working build system Right now it compiles react and uses scss for styling It's still missing a linter and a test environment but it's good enough to start working.

---
+++ src/home-page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <header role='banner'>
+          <h1>Meson Baturro</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default HomePage
