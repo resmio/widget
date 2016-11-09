6034248a1ef08f818d8978af13a2db54f1e11723
menor <josemenor@gmail.com>
2016-11-09 14:26:02 +0100

    Add mini router

--- src/containers/BookingPanel.jsx
+++ src/containers/BookingPanel.jsx
@@ -1,61 +1,66 @@
 import React, { Component, PropTypes } from 'react'
-import { momentObj } from 'react-moment-proptypes'
 import { render } from 'react-dom'
-import { SingleDatePicker } from 'react-dates'
-import 'react-dates/lib/css/_datepicker.css'
-import NumberSelector from '../components/NumberSelector'
-import Header from '../components/Header'
+// import { momentObj } from 'react-moment-proptypes'
+// import { SingleDatePicker } from 'react-dates'
+// import 'react-dates/lib/css/_datepicker.css'
+// import NumberSelector from '../components/NumberSelector'
+// import Header from '../components/Header'
 
 class BookingPanel extends Component {
 
-  constructor (props) {
-    super(props)
-  }
-
   render () {
-    const {leDate, openCalendar, onCalendarFocusChange, onDateChange} = this.props
     return (
-      <div className='widget-container'>
-        <Header
-          collapsed
-          facility='Meson Baturro'
-          bgImage="https://zenezake.files.wordpress.com/2015/07/img_6715.jpg"
-        />
-        <section className="main-section">
-          <NumberSelector />
-          <section className='calendar'>
-            <span>Date</span>
-            <SingleDatePicker
-              id="date_input"
-              date={leDate}
-              focused={openCalendar}
-              onFocusChange={onCalendarFocusChange}
-              numberOfMonths={1}
-              onDateChange={onDateChange}
-            />
-          </section>
-          <section className='time__selector'>
-            <span>Time</span>
-            <span>18:00</span>
-            <span>20%</span>
-          </section>
-        </section>
-        <footer>
-          <span>Mierdilogo</span>
-          <input type="button" value="Book Now"/>
-        </footer>
-      </div>
+      <h1>Booking Panel</h1>
     )
   }
+  // constructor (props) {
+  //   super(props)
+  // }
+  //
+  // render () {
+  //   const {leDate, openCalendar, onCalendarFocusChange, onDateChange} = this.props
+  //   return (
+  //     <div className='widget-container'>
+  //       <Header
+  //         collapsed
+  //         facility='Meson Baturro'
+  //         bgImage="https://zenezake.files.wordpress.com/2015/07/img_6715.jpg"
+  //       />
+  //       <section className="main-section">
+  //         <NumberSelector />
+  //         <section className='calendar'>
+  //           <span>Date</span>
+  //           <SingleDatePicker
+  //             id="date_input"
+  //             date={leDate}
+  //             focused={openCalendar}
+  //             onFocusChange={onCalendarFocusChange}
+  //             numberOfMonths={1}
+  //             onDateChange={onDateChange}
+  //           />
+  //         </section>
+  //         <section className='time__selector'>
+  //           <span>Time</span>
+  //           <span>18:00</span>
+  //           <span>20%</span>
+  //         </section>
+  //       </section>
+  //       <footer>
+  //         <span>Mierdilogo</span>
+  //         <input type="button" value="Book Now"/>
+  //       </footer>
+  //     </div>
+  //   )
+  // }
 }
 
-const { bool,func } = PropTypes
-
-BookingPanel.propTypes = {
-  leDate: momentObj,
-  openCalendar: bool,
-  onCalendarFocusChange: func,
-  onDateChange: func
-}
+// const { bool,func } = PropTypes
+//
+// BookingPanel.propTypes = {
+//   leDate: momentObj,
+//   openCalendar: bool,
+//   onCalendarFocusChange: func,
+//   onDateChange: func
+// }
 
 export default BookingPanel
