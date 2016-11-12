6dc9b79df94f44b3e385d63b42ca0362fdc30d8b
menor <josemenor@gmail.com>
2016-11-12 11:52:39 +0100

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 import App from './App';
-import './index.css';
+import './styles/index';
 
 const MOUNT_NODE = document.getElementById('root')
 const app = (
