b1d71231f2bb78c8479ca5abf9dc5a28f070962e
Niels Sandholt Busch <niels.busch@gmail.com>
2017-05-31 11:43:05 +0200

    remove unused code, refs #62

--- scripts/translate.js
+++ scripts/translate.js
@@ -8,30 +8,14 @@ const globSync = require('glob').sync;
 const mkdirpSync = require('mkdirp').sync;
 const transformFileSync = require('babel-core').transformFileSync;
 
-// const paths = require('../config/paths');
 const paths = {
   appSrc: 'src',
   appBuildMessages: 'build/messages'
 }
-
-//////////////////////////////
-// Add to `/config/paths.js`:
-//  appBuildMessages: .. where to output the messages
-//////////////////////////////
 
 globSync(path.join(paths.appSrc, '/**/*.jsx'))
     .map((filename) => {
         const result = transformFileSync(filename, {
             plugins: ['react-intl']
         });
-        // const messages = result.metadata["react-intl"]["messages"];
-        // if (messages.length > 0) {
-        //     const relFn = path.relative(paths.appSrc, filename);
-        //     const outFilename = path.join(
-        //         paths.appBuildMessages,
-        //         path.dirname(relFn), `${path.basename(relFn, ".js")}.json`);
-        //     mkdirpSync(path.dirname(outFilename));
-        //     const outFd = fs.openSync(outFilename, 'w');
-        //     fs.write(outFd, JSON.stringify(messages));
-        // }
     });
