fadb8047c451472fe861e47dda3d768b1f251597
Niels Sandholt Busch <niels.busch@gmail.com>
2017-05-31 11:45:14 +0200

    remove unused script, refs #62

--- scripts/extractMessages.js
+++
@@ -1,7 +0,0 @@
-var babel = require('babel-core');
-
-const result = babel.transform('src', {
-  plugins: ['react-intl']
-}) // => { code, map, ast, metadata['react-intl'].messages };
-
-console.log(result)
