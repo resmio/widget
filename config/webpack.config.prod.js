38a56e3bdf9dc78766f85a0beeb3b049452a0a26
Niels Sandholt Busch <niels.busch@gmail.com>
2017-06-27 13:09:51 +0200

    fix css loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,7 +1,7 @@
 var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var ManifestPlugin = require('webpack-manifest-plugin');
 var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 var url = require('url');
@@ -144,11 +144,12 @@ module.exports = {
       // tags. If you use code splitting, however, any async bundles will still
       // use the "style" loader inside the async code so CSS from them won't be
       // in the main CSS file.
-      // {
-      //   test: /\.css$/,
-      //   loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
-      //   // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
-      // },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
+        // loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
+        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
@@ -228,7 +229,7 @@ module.exports = {
       }
     }),
     // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    // new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
+    new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
